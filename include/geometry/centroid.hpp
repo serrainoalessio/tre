ac03eafafdd3f38f50243bce7e0906ff7abff438
Alessio Serraino <serrainoalesio@debian>
2016-01-23 11:08:28 +0100

    Addedd all point members but centroids :disappointed:

---
+++ include/geometry/centroid.hpp
@@ -0,0 +1,3 @@
+class point_centroid() {
+    
+}
--- src/point/point.cpp
+++ src/point/point.cpp
@@ -3,5 +3,7 @@
 
 // include all function definitions
 #include "geometry/point/point_operators.hpp"
+#include "geometry/point/point_distance.hpp"
+#include "geometry/point/point_aligned.hpp"
 
 template<> class point<float, 2>; // explicit template instantiation
